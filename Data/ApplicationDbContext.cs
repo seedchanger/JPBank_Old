1329979cb84b1fd61762202c15bb37bc16f9a1dc
John <johnpaulfrancis@gmail.com>
2024-09-21 20:12:26 +0530

    Installed 3 Nuget Packages (EFCore, EFCore.SqlServer, EFCore.Tools) and Created a class file for Dependancy Injection

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JPBank.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
