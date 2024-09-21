1fe35a47dcb07a448df050f581a309bec7162f25
John <johnpaulfrancis@gmail.com>
2024-09-21 22:32:32 +0530

    This code defines a property UsersDbSet that will hold the rows of the Users table, with each row being an instance of the UserDbEntity class. We can perform CRUD operations on the Users table through this DbSet

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JPBank.Models.DbModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace JPBank.Data
 {
@@ -8,5 +9,7 @@ namespace JPBank.Data
         {
             
         }
+
+        public DbSet<UserDbEntity> UsersDbSet { get; set; }
     }
 }
