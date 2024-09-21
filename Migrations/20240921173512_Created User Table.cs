7267bbd27375350caeea28d76e90ec9625d7957e
John <johnpaulfrancis@gmail.com>
2024-09-21 23:07:06 +0530

    Modified the UserDbEntity class and migrated the tables using following command type in Nuget Package Manager Console 1. Add-Migration 'Created User Table' 2. Update-Database

---
+++ Migrations/20240921173512_Created User Table.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JPBank.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersDbSet",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserType = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersDbSet", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersDbSet");
+        }
+    }
+}
--- Models/DbModels/UserDbEntity.cs
+++ Models/DbModels/UserDbEntity.cs
@@ -1,11 +1,19 @@
-﻿namespace JPBank.Models.DbModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JPBank.Models.DbModels
 {
     public class UserDbEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
-        public required string Name { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
         public int UserType { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedOn { get; set; }
