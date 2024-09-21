0fdb35d40d937fa8728b38edd54a3533bfc9363b
John <johnpaulfrancis@gmail.com>
2024-09-21 22:00:42 +0530

    Created UserEntity Class

---
+++ Models/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace JPBank.Models
+{
+    public class UserEntity
+    {
+        public int UserId { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public int UserType { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
